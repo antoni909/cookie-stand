61dc0d9cacc7a633ee0a20bf832afbcb0b23408f
antoni <lorenzo.antonio.090@gmail.com>
2021-02-01 08:25:09 -0800

    complete logic requirements for assignment and have working table to display on live link

--- js/app.js
+++ js/app.js
@@ -1,14 +1,48 @@
 'use strict';
 
 // 1.Create Element --> 2.Give Content --> 3.Append-to-Container
+// prototypes assigns default methods to all instances
+// all instances can be rendered in allStoreCreators
 
-let myTableContainer = document.getElementById('container');
-
+document.getElementById('container');
 let cookieTable = document.getElementById('cookie-table');
-
+let arrHours = ['06:00 am','07:00 am','08:00 am','09:00 am','10:00 am','11:00 am','12:00 am','01:00 pm','02:00 pm', '03:00 pm', '04:00 pm', '05:00 pm', '06:00 pm', '07:00 pm'];
 const allStoreCreator = [];
 
-let arrHours = ['06:00 am','07:00 am','08:00 am','09:00 am','10:00 am','11:00 am','12:00 am','01:00 pm','02:00 pm', '03:00 pm', '04:00 pm', '05:00 pm', '06:00 pm', '07:00 pm'];
+function renderAll() {
+
+  for(var i=0;i< allStoreCreator.length; i++){
+    allStoreCreator[i].render();
+  }
+}
+
+function tableHeader() {
+  let thead = document.createElement('thead');
+  cookieTable.appendChild(thead);
+  let tr = document.createElement('tr');
+  let th = document.createElement('th');
+  th.textContent = '';
+  thead.appendChild(tr);
+  tr.appendChild(th);
+
+  for(var i =0; i<arrHours.length;i++){
+    let td = document.createElement('td');
+    td.textContent = arrHours[i];
+    tr.appendChild(td);
+  }
+
+  let thTotal = document.createElement('th');
+  thTotal.textContent = 'Daily Location Total';
+  tr.appendChild(thTotal);
+}
+
+function tableTotalRow() {
+  let tfoot = document.createElement('tfoot');
+  cookieTable.appendChild(tfoot);
+  let th = document.createElement('th');
+  th.textContent = 'Totals';
+  tfoot.appendChild(th);
+}
 
 function StoreCreator (location, minCust, maxCust, averageCookiesPerCust){
   this.location = location;
@@ -16,10 +50,9 @@ function StoreCreator (location, minCust, maxCust, averageCookiesPerCust){
   this.maxCust = maxCust;
   this.averageCookiesPerCust = averageCookiesPerCust;
   this.arrayCookiesSoldPerHour = [];
-  this.totalCookies = 0;
+  this.sumCookiesTotal = 0;
   allStoreCreator.push(this);
 }
-//all instances can be rendered in allStoreCreators
 
 StoreCreator.prototype.randomCustPerHour = function(){
   return Math.floor (Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
@@ -49,55 +82,24 @@ StoreCreator.prototype.render = function(){
   for(var i=0;i<arrHours.length;i++){
     let td = document.createElement('td');
     td.textContent = this.arrayCookiesSoldPerHour[i];
-    tr.appendChild(td);
 
-    // this.arrayCookies();
-    //might have to change uls to proper tags
-    // for(var i=0;i<this.arrayCookiesSoldPerHour.length;i++){
-    //   let ul = document.createElement('ul');
-    //   ul.textContent = `${arrHours[i]}: ${this.arrayCookiesSoldPerHour[i]} cookies`;
-    //   ulLima.appendChild(ul);
-    // }
-    // let totalCookies = document.createElement('ul');
-    // totalCookies.textContent =`Total: ${this.sumCookiesTotal} cookies` ;
-    // ulLima.appendChild(totalCookies);
+    tr.appendChild(td);
   }
+  let td = document.createElement('td');
+  td.textContent = this.sumCookiesTotal;
+  tr.appendChild(td);
 };
 
-// you can add properies by dot chain to the instance desired
-// prototypes assigns default methods to all instances
+new StoreCreator('seattle', 23, 65, 6.3);
 
-function renderAll() {
-  for(var i=0;i< allStoreCreator.length; i++){
-    allStoreCreator[i].render();
-  }
-}
+new StoreCreator('tokyo', 3, 24, 1.2);
 
-let seattleShop = new StoreCreator('seattle', 23, 65, 6.3);
+new StoreCreator('dubai', 11, 38, 3.7);
 
-let tokyoShop = new StoreCreator('tokyo', 3, 24, 1.2);
+new StoreCreator('paris', 20, 38, 2.3);
 
-let dubaiShop = new StoreCreator('dubai', 11, 38, 3.7);
-
-let parisShop = new StoreCreator('paris', 20, 38, 2.3);
-
-let limaShop = new StoreCreator('lima', 2, 16, 4.6);
-
+new StoreCreator('lima', 2, 16, 4.6);
 
 renderAll();
-
-// create multiple tds, give conten append to dom
-
-// for (let i=0; i ){
-// var td = document.createElement('td');
-// td.textContent = this.yield[i];
-// true.appendChild(td);
-// this.total +=  this. some yield
-// }
-
-// create total td, give content, append to dom
-
-// let td = document.createElement('td');
-// td.textContent = this.total;
-// tr.appendChild(td);
-// put render functions into array
+tableHeader();
+tableTotalRow();
